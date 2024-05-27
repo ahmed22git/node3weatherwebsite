e1e5d764945b11c4ad062e65ad45a00e85eadce9
ahmed22git <145002344+ahmed22git@users.noreply.github.com>
2024-05-27 09:16:55 +0500

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 
 console.log(__dirname)
@@ -107,6 +108,6 @@ app.get('*',(req,res) =>{
 })
 
 
-app.listen(3000 , () =>{
+app.listen(port , () =>{
     console.log('Start correctly')
 })
